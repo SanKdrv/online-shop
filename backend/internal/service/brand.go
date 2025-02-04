10b687a14e07c744c76aa7623520d0cb0c0a0770
SanKdrv <kudryash2005@gmail.com>
2025-02-04 18:51:02 +0300

    A little bit of documentation + refactoring names of variables

--- backend/internal/service/brand.go
+++ backend/internal/service/brand.go
@@ -10,22 +10,37 @@ func NewBrandsService(repo repository.Brands) *BrandsService {
 	return &BrandsService{repo: repo}
 }
 
+// GetIDByBrand retrieves the ID of a brand by its name.
+//
+// Returns the brand ID and an error if the operation fails or if the brand does not exist.
 func (s *BrandsService) GetIDByBrand(name string) (int64, error) {
-	return 0, nil
+	return s.repo.GetIDByBrand(name)
 }
 
-func (s *BrandsService) GetBrandByID(categoryID int64) (string, error) {
-	return "", nil
+// GetBrandByID returns brand name by brand id.
+//
+// Returns empty string if brand does not exist in DB.
+func (s *BrandsService) GetBrandByID(brandId int64) (string, error) {
+	return s.repo.GetBrandByID(brandId)
 }
 
+// CreateBrand creates a new brand with the given name.
+//
+// Returns the ID of the newly created brand and an error, if any occurred during creation.
 func (s *BrandsService) CreateBrand(name string) (int64, error) {
-	return 0, nil
+	return s.repo.CreateBrand(name)
 }
 
-func (s *BrandsService) DeleteBrand(categoryID int64) error {
-	return nil
+// DeleteBrand deletes a brand with the given ID.
+//
+// Returns an error if the deletion fails or if the brand does not exist.
+func (s *BrandsService) DeleteBrand(brandId int64) error {
+	return s.repo.DeleteBrand(brandId)
 }
 
-func (s *BrandsService) UpdateBrand(categoryID int64, name string) error {
-	return nil
+// UpdateBrand updates the name of the brand with the given ID.
+//
+// Returns an error if the update fails or if the brand does not exist.
+func (s *BrandsService) UpdateBrand(brandId int64, name string) error {
+	return s.repo.UpdateBrand(brandId, name)
 }
