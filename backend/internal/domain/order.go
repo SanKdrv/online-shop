ffbc7434c2ba13e2d35bd21204ba10ddf1c951bd
SanKdrv <kudryash2005@gmail.com>
2025-03-12 00:33:57 +0300

    Order basic fix

--- backend/internal/domain/order.go
+++ backend/internal/domain/order.go
@@ -4,7 +4,7 @@ import "time"
 
 type Order struct {
 	Id        int64     `db:"id" json:"id,omitempty"`
-	OrderId   int64     `db:"order_id" json:"order_id,omitempty"`
+	UserId    int64     `db:"user_id" json:"user_id,omitempty"`
 	Status    string    `db:"status" json:"status,omitempty"`
 	CreatedAt time.Time `db:"created_at" json:"created_at,omitempty"`
 	UpdatedAt time.Time `db:"updated_at" json:"updated_at,omitempty"`
